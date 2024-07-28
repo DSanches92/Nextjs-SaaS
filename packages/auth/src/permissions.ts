1a1e7baeb2d3cb0c9f409de93bd9ff4536b8bdad
Danilo Sanches <silva.danilosanches@gmail.com>
2024-07-28 15:25:26 -0300

    base commit inicial
    Commit inicial do projeto Next.js SaaS que estou desenvolvendo no curso da Rocketseat.

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,31 @@
+import { AbilityBuilder } from '@casl/ability'
+
+import { AppAbility } from '.'
+import { User } from './models/user'
+import { Role } from './roles'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  admin: (user, { can, cannot }) => {
+    can('manage', 'all')
+
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    })
+  },
+
+  member: (user, { can }) => {
+    can('read', 'User')
+    can(['create', 'read'], 'Project')
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } })
+  },
+
+  billing: (_, { can }) => {
+    can('manage', 'Billing')
+  },
+}
